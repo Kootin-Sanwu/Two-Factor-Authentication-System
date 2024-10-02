11d9460411406cbda89a26282455f58b1c818fad
Kootin-Sanwu <kobekootinsanwu@gmail.com>
2024-10-02 15:45:18 +0000

    "Extracted JavaScript code from login.php to a separate file login.js"

--- view/login.php
+++ view/login.php
@@ -75,22 +75,7 @@
         </div>
     </div>
     
-    <!-- js code -->
-    <script>
-        const container = document.getElementById('container');
-        const overlayCon = document.getElementById('overlayCon');
-        const overlayBtn = document.getElementById('overlayBtn');
-
-        overlayBtn.addEventListener('click', () => {
-            container.classList.toggle('right-panel-active');
-
-            overlayBtn.classList.remove('btnScaled');
-            window.requestAnimationFrame(() => {
-                overlayBtn.classList.add('btnScaled');
-            });
-        });
-    </script>
-
+    <script src="../js/login.js"></script>
 
 </body>
 </html>
